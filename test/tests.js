78064980c63f0f8c36be2a8e9812f287d94b5897
Venkatraman <ramsunvtech@gmail.com>
2014-06-26 15:22:12 -0500

    Test Case

--- test/tests.js
+++ test/tests.js
@@ -1,42 +1,40 @@
 var myProfile = new Backbone.Model({
-    defaults: {
-        'firstname': 'Venkatraman',
-        'lastname': 'Ramamoorthy',
-        'phone': {
-            'mobile': '+91-9902773692',
-            'landline': '+91-3404353453',
-            'office': '+91-4402773345'
-        },
-        'addressBook': [{
-            'street': '7a, Munnusamy Nagar',
-            'area': 'Kolathur',
-            'city': 'Chennai',
-            'state': 'Tamilnadu',
-            'type': 'last'
-        }, {
-            'street': '36, Manika Vinayagar Koil street',
-            'area': 'Permabur',
-            'city': 'Chennai',
-            'state': 'Tamilnadu',
-            'type': 'permanent'
-        }, {
-            'street': '10/1, Venkatapura 11th Main road',
-            'area': 'Koramangala',
-            'city': 'Bangalore',
-            'state': 'Karnataka',
-            'type': 'last'
-        }, {
-            'street': '100/34, Third C Cross Road',
-            'area': 'Koramangala',
-            'city': 'Bangalore',
-            'state': 'Karnataka',
-            'type': 'current'
-        }]
-    }
+    'firstname': 'Venkatraman',
+    'lastname': 'Ramamoorthy',
+    'phone': {
+        'mobile': '+91-9902773692',
+        'landline': '+91-3404353453',
+        'office': '+91-4402773345'
+    },
+    'addressBook': [{
+        'street': '7a, Munnusamy Nagar',
+        'area': 'Kolathur',
+        'city': 'Chennai',
+        'state': 'Tamilnadu',
+        'type': 'last'
+    }, {
+        'street': '36, Manika Vinayagar Koil street',
+        'area': 'Permabur',
+        'city': 'Chennai',
+        'state': 'Tamilnadu',
+        'type': 'permanent'
+    }, {
+        'street': '10/1, Venkatapura 11th Main road',
+        'area': 'Koramangala',
+        'city': 'Bangalore',
+        'state': 'Karnataka',
+        'type': 'last'
+    }, {
+        'street': '100/34, Third C Cross Road',
+        'area': 'Koramangala',
+        'city': 'Bangalore',
+        'state': 'Karnataka',
+        'type': 'current'
+    }]
 });
 
-test("get one attribute", function () {
-    equal(myProfile.select('firstname'), {
+test( "get one attribute", function( assert ) {
+  assert.ok( myProfile.select('firstname') == {
         firstname: "Venkatraman"
-    });
+    }, "Passed!" );
 });
